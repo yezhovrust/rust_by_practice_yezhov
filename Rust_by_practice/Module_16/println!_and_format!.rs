c727644d6415ec0dd5d2f39dafe468ff369d5e71
yezhovrust <v.yezhov@e-u.edu.ua>
2024-11-13 22:16:15 +0200

    other modules commit

---
+++ Rust_by_practice/Module_16/println!_and_format!.rs
@@ -0,0 +1,16 @@
+
+fn main() {
+    let s1 = "hello";
+    /* Fill in the blank */
+    let s = format!("{}, world!", s1);
+    assert_eq!(s, "hello, world!");
+}
+
+
+
+
+fn main() {
+    print!("hello world, ");
+    println!("I am");
+    println!("Sunface!");
+}
